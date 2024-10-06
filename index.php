46fb1f5bda00960eb34957947e1cfd124ce4d679
unknown <willreubena@gmail.com>
2024-10-06 19:01:48 +0100

    added homepage css

--- index.php
+++ index.php
@@ -19,7 +19,9 @@ $user = $usersModel->getUserById(1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Task Homepage</title>
+
+    <link rel="stylesheet" href="styles/homepage.css">
 </head>
 <body>
     <header>
@@ -30,11 +32,21 @@ $user = $usersModel->getUserById(1);
         <a>Login</a>
     </header>
     <h1>Welcome <?php echo $user->getUsername(); ?> to your To-Do Page!<h1>
-    <div>
-        <div>Notifications:</div>
-    </div>
-    <div>
-        <div>Due soon:</div>
+    <div class="notifications-and-due">
+        <div class="notifications-container">
+            <div class="info-box title-box">Notifications:</div>
+            <div class="info-box">Story 3 in Taskboard 4 edited at 13:43</div>
+            <div class="info-box">New Task "Create front-end" added to Taskboard 1</div>
+            <div class="info-box">New Task "Story 6" added to Taskboard 2</div>
+            <div class="info-box">Task "Story 1" moved to Completed on Taskboard 1</div>
+        </div>
+        <div class="notifications-container">
+            <div class="info-box title-box">Due soon:</div>
+            <div class="info-box">Story 3 in Taskboard 4</div>
+            <div class="info-box">Story 5 in Taskboard 4</div>
+            <div class="info-box">Story 5 in Taskboard 1</div>
+            <div class="info-box">Story 6 in Taskboard 1</div>
+        </div>
     </div>
 </body>
 </html>
